be4472ed6584550a5637b13ac8993f9d20964cae
devjohn42 <devjohncb@gmail.com>
2026-06-02 21:14:36 -0300

    :sparkles: feat: initial structure for the circuit-breaker

---
+++ src/common/circuit-breaker/circuit-breaker.interface.ts
@@ -0,0 +1,25 @@
+enum CircuitBreakerStateEnum {
+	CLOSED = 'CLOSED',
+	OPEN = 'OPEN',
+	HALF_OPEN = 'HALF_OPEN'
+}
+
+export interface CircuitBreakerOptions {
+	failureThreshold: number
+	timeout: number
+	resetTimeout: number
+}
+
+export interface CircuitBreakerState {
+	state: CircuitBreakerStateEnum
+	failureCount: number
+	lastFailureTime: number
+	nextAttemptTime: number
+}
+
+export interface CircuitBreakerResult<T> {
+	success: boolean
+	data?: T
+	error?: Error
+	fromCache?: boolean
+}
